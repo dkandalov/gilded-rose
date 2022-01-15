0d4c67e6bad2eb7ba4a11223d9d60f09b7ed9628
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-01-15 10:27:20 +0000

    tidy up

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -3,17 +3,12 @@ package com.gildedrose
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class GildedRoseTest {
-
+class GildedRoseTest {
     @Test
     fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+        val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+        assertEquals("foo", app.items.first().name)
     }
-
 }
-
-
