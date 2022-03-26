2f9fea938cb1b14275f1932848897ee05eb6ca85
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-03-26 16:09:37 +0000

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -9,31 +9,27 @@ class GildedRose(val items: Array<Item>) {
 }
 
 private fun Item.updateQuality() {
-    if (name == "Aged Brie") {
-        if (quality < 50) quality += 1
-    } else if (name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (quality < 50) {
-            quality += 1
-            if (sellIn < 11 && quality < 50) quality += 1
-            if (sellIn < 6 && quality < 50) quality += 1
+    when (name) {
+        "Aged Brie"                                 -> if (quality < 50) quality += 1
+        "Backstage passes to a TAFKAL80ETC concert" -> {
+            if (quality < 50) {
+                quality += 1
+                if (sellIn < 11 && quality < 50) quality += 1
+                if (sellIn < 6 && quality < 50) quality += 1
+            }
         }
-    } else if (name == "Sulfuras, Hand of Ragnaros") {
-        quality -= 0
-    } else {
-        if (quality > 0) quality -= 1
+        "Sulfuras, Hand of Ragnaros"                -> quality -= 0
+        else                                        -> if (quality > 0) quality -= 1
     }
 
     if (name != "Sulfuras, Hand of Ragnaros") sellIn -= 1
 
     if (sellIn < 0) {
-        if (name == "Aged Brie") {
-            if (quality < 50) quality += 1
-        } else if (name == "Backstage passes to a TAFKAL80ETC concert") {
-            quality = 0
-        } else if (name == "Sulfuras, Hand of Ragnaros") {
-            quality -= 0
-        } else {
-            if (quality > 0) quality -= 1
+        when (name) {
+            "Aged Brie"                                 -> if (quality < 50) quality += 1
+            "Backstage passes to a TAFKAL80ETC concert" -> quality = 0
+            "Sulfuras, Hand of Ragnaros"                -> quality -= 0
+            else                                        -> if (quality > 0) quality -= 1
         }
     }
 }
