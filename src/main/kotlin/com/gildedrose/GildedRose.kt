38c8a9abf3474be6cb1ea59c51cce0a425d0c952
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-03-26 15:59:34 +0000

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,53 +6,53 @@ class GildedRose(var items: Array<Item>) {
             items[i].updateQuality()
         }
     }
+}
 
-    private fun Item.updateQuality() {
-        if (name != "Aged Brie" && name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (quality > 0) {
-                if (name != "Sulfuras, Hand of Ragnaros") {
-                    quality -= 1
+private fun Item.updateQuality() {
+    if (name != "Aged Brie" && name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (quality > 0) {
+            if (name != "Sulfuras, Hand of Ragnaros") {
+                quality -= 1
+            }
+        }
+    } else {
+        if (quality < 50) {
+            quality += 1
+
+            if (name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (sellIn < 11) {
+                    if (quality < 50) {
+                        quality += 1
+                    }
                 }
+
+                if (sellIn < 6) {
+                    if (quality < 50) {
+                        quality += 1
+                    }
+                }
+            }
+        }
+    }
+
+    if (name != "Sulfuras, Hand of Ragnaros") {
+        sellIn -= 1
+    }
+
+    if (sellIn < 0) {
+        if (name != "Aged Brie") {
+            if (name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (quality > 0) {
+                    if (name != "Sulfuras, Hand of Ragnaros") {
+                        quality -= 1
+                    }
+                }
+            } else {
+                quality = 0
             }
         } else {
             if (quality < 50) {
                 quality += 1
-
-                if (name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality += 1
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality += 1
-                        }
-                    }
-                }
-            }
-        }
-
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn -= 1
-        }
-
-        if (sellIn < 0) {
-            if (name != "Aged Brie") {
-                if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (quality > 0) {
-                        if (name != "Sulfuras, Hand of Ragnaros") {
-                            quality -= 1
-                        }
-                    }
-                } else {
-                    quality = 0
-                }
-            } else {
-                if (quality < 50) {
-                    quality += 1
-                }
             }
         }
     }
