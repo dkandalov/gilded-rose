865d95ef68d0dfabfd05241039027a16ca4e2a58
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-03-26 16:28:49 +0000

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -11,24 +11,20 @@ class GildedRose(val items: Array<Item>) {
 private fun Item.update() {
     sellIn -= if (name == "Sulfuras, Hand of Ragnaros") 0 else 1
 
-    updateQuality(
-        when (name) {
-            "Aged Brie"                                 -> if (expired) 2 else 1
-            "Backstage passes to a TAFKAL80ETC concert" ->
-                if (expired) -quality
-                else if (sellIn < 5) 3
-                else if (sellIn < 10) 2
-                else 1
-            "Sulfuras, Hand of Ragnaros"                -> 0
-            else                                        -> if (expired) -2 else -1
-        }
-    )
+    val qualityChange = when (name) {
+        "Aged Brie"                                 -> if (expired) 2 else 1
+        "Backstage passes to a TAFKAL80ETC concert" ->
+            if (expired) -quality
+            else if (sellIn < 5) 3
+            else if (sellIn < 10) 2
+            else 1
+        "Sulfuras, Hand of Ragnaros"                -> 0
+        else                                        -> if (expired) -2 else -1
+    }
+    if (qualityChange != 0) {
+        quality = (quality + qualityChange).coerceIn(minimumValue = 0, maximumValue = 50)
+    }
 }
 
 private val Item.expired get() = sellIn < 0
-
-private fun Item.updateQuality(qualityChange: Int) {
-    if (qualityChange == 0) return
-    quality = (quality + qualityChange).coerceIn(minimumValue = 0, maximumValue = 50)
-}
 
