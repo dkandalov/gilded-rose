4063d4187405ce7e53f9a0247803029324aa3f8d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-03-26 16:04:31 +0000

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -11,49 +11,29 @@ class GildedRose(val items: Array<Item>) {
 private fun Item.updateQuality() {
     if (name != "Aged Brie" && name != "Backstage passes to a TAFKAL80ETC concert") {
         if (quality > 0) {
-            if (name != "Sulfuras, Hand of Ragnaros") {
-                quality -= 1
-            }
+            if (name != "Sulfuras, Hand of Ragnaros") quality -= 1
         }
     } else {
         if (quality < 50) {
             quality += 1
-
             if (name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (sellIn < 11) {
-                    if (quality < 50) {
-                        quality += 1
-                    }
-                }
-
-                if (sellIn < 6) {
-                    if (quality < 50) {
-                        quality += 1
-                    }
-                }
+                if (sellIn < 11 && quality < 50) quality += 1
+                if (sellIn < 6 && quality < 50) quality += 1
             }
         }
     }
 
-    if (name != "Sulfuras, Hand of Ragnaros") {
-        sellIn -= 1
-    }
+    if (name != "Sulfuras, Hand of Ragnaros") sellIn -= 1
 
     if (sellIn < 0) {
         if (name != "Aged Brie") {
             if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (quality > 0) {
-                    if (name != "Sulfuras, Hand of Ragnaros") {
-                        quality -= 1
-                    }
-                }
+                if (quality > 0 && name != "Sulfuras, Hand of Ragnaros") quality -= 1
             } else {
                 quality = 0
             }
         } else {
-            if (quality < 50) {
-                quality += 1
-            }
+            if (quality < 50) quality += 1
         }
     }
 }
