117d22f3e51b99e0faa25db5312868e54125ef54
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-10-05 10:20:22 +0100

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,48 +4,30 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
+            update(item)
+        }
+    }
+}
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
+private fun update(item: Item) {
+    if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
+                item.quality = item.quality - 1
             }
+        }
+    } else {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
 
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
-                } else {
+                }
+
+                if (item.sellIn < 6) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
                     }
@@ -54,5 +36,26 @@ class GildedRose(var items: Array<Item>) {
         }
     }
 
+    if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.sellIn = item.sellIn - 1
+    }
+
+    if (item.sellIn < 0) {
+        if (item.name != "Aged Brie") {
+            if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > 0) {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.quality = item.quality - 1
+                    }
+                }
+            } else {
+                item.quality = 0
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+        }
+    }
 }
 
