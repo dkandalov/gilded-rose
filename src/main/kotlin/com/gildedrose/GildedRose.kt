1c56ec2df064511e53bbbd029c49e0e955df85c3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-11-03 19:17:15 +0000

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -30,16 +30,11 @@ private fun Item.update() {
     if (name != "Sulfuras, Hand of Ragnaros") sellIn -= 1
 
     if (sellIn < 0) {
-        if (name != "Aged Brie") {
-            if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (quality > 0) {
-                    if (name != "Sulfuras, Hand of Ragnaros") quality -= 1
-                }
-            } else {
-                quality = 0
-            }
-        } else {
-            if (quality < 50) quality += 1
+        when (name) {
+            "Aged Brie" -> if (quality < 50) quality += 1
+            "Backstage passes to a TAFKAL80ETC concert" -> quality = 0
+            "Sulfuras, Hand of Ragnaros" -> quality -= 0
+            else -> if (quality > 0) quality -= 1
         }
     }
 }
