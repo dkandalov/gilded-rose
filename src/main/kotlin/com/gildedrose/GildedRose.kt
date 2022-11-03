00612003abac41ae9350fb2b8437b4dce22a1b1e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-11-03 19:19:29 +0000

    refactoring

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -9,22 +9,27 @@ class GildedRose(var items: Array<Item>) {
 }
 
 private fun Item.update() {
-    if (name == "Aged Brie") {
-        if (quality < 50) quality += 1
-    } else if (name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (quality < 50) {
-            quality += 1
-            if (sellIn < 11) {
-                if (quality < 50) quality += 1
-            }
-            if (sellIn < 6) {
-                if (quality < 50) quality += 1
+    when (name) {
+        "Aged Brie" -> {
+            if (quality < 50) quality += 1
+        }
+        "Backstage passes to a TAFKAL80ETC concert" -> {
+            if (quality < 50) {
+                quality += 1
+                if (sellIn < 11) {
+                    if (quality < 50) quality += 1
+                }
+                if (sellIn < 6) {
+                    if (quality < 50) quality += 1
+                }
             }
         }
-    } else if (name == "Sulfuras, Hand of Ragnaros") {
-        quality -= 0
-    } else {
-        if (quality > 0) quality -= 1
+        "Sulfuras, Hand of Ragnaros" -> {
+            quality -= 0
+        }
+        else -> {
+            if (quality > 0) quality -= 1
+        }
     }
 
     sellIn -= when (name) {
